4b4ccdf521e1a47afed046818c578bd0d0f7d4d8
Bret Swedeen <bret@pobox.com>
2018-01-08 13:51:21 -0500

    Updates to indexnew

--- indexnew.php
+++ indexnew.php
@@ -6,8 +6,9 @@ ini_set('display_errors', 1);
 include_once("config.php");
 $action = 0;
 $result = mysqli_query($mysqli, "SELECT * FROM User ORDER BY idUser DESC");
-if(!empty($_GET['action'] && !empty($_GET['id'])){
+if((!empty($_GET['action'])) && (!empty($_GET['id'])) {
     $action = $_GET['action'];
+    $id = $_GET['id'];
     }   
 ?>
 
@@ -35,10 +36,28 @@ if(!empty($_GET['action'] && !empty($_GET['id'])){
 	<?php 
 	//while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
 	while($res = mysqli_fetch_array($result)) {
-        if(($action == 1) && ($idact == $res['idUser'])){
-            echo "<tr>";
-            echo "<td>EDIT ROW</td>";
-            echo "<tr>";
+        if(($action == 1) && ($id == $res['idUser'])){
+            //echo "<tr>";
+            //echo "<td>EDIT ROW</td>";
+            //echo "<tr>";
+    ?>        
+        <form name="form1" method="post" action="update.php">
+		<table border="0">
+			<tr> 
+				<td><input type="text" name="Forename" value="<?php echo $res['Forename']?>"></td>
+				<td><input type="text" name="Surname" value="<?php echo $res['Surname']?>"></td>
+				<td><input type="text" name="Department" value="<?php echo $res['Department'];?>"></td>
+				<td><input type="text" name="Catalouging" value="<?php echo $res['Catalouging'];?>"></td>
+				<td><input type="text" name="Assessments" value="<?php echo $res['Assessments'];?>"></td>
+				<td><input type="text" name="Loans" value="<?php echo $res['Loans'];?>"></td>
+				<td><input type="text" name="Admins" value="<?php echo $res['Admins'];?>"></td>
+				<td><input type="text" name="UserPassword" value="<?php echo $res['Password'];?>"></td>
+				<td><input type="hidden" name="idUser" value="<?php echo $idUser;?>"></td>
+				<td><input type="submit" value="Update"></td>
+			</tr>
+		</table>
+    </form>
+    <?php
         } else{
             echo "<tr>";
             echo "<td>".$res['ForeName']."</td>";
